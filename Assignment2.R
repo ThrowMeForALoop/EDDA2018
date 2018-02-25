998d5fa5c58365f728ca47252b8293beff273af3
sarasal <sara.sarnv95@gmail.com>
2018-02-25 13:09:59 +0100

    Merged Exercise 3-6-7

--- Assignment2.R
+++ Assignment2.R
@@ -1,4 +1,4 @@
-# Ex 2
+### Exercise 2
 light1879_dataframe=read.table("light1879.txt", header = FALSE)
 light1882_dataframe=read.table("light1882.txt", fill = TRUE)
 light_dataframe= read.table("light.txt", header = FALSE)
@@ -13,7 +13,7 @@ light1882_vec = light1882_vec + 299000
 light_vec = unlist(light_dataframe, use.names = FALSE)
 light_vec = 7.442 / (((light_vec/1000) + 24.8)/1000000)
 
-#Question 2.1
+# Question 2.1
 par(mfrow=c(1,2))
 
 # Answer: Given the boxplot and histogram of light1879 dataset, we can observe some outliers and the qqline is not straight
@@ -39,7 +39,7 @@ boxplot(light_vec)
 qqnorm(light_vec)
 qqline(light_vec)
 
-#Question 2.2
+# Question 2.2
 # Dataset 1879
 #### MEAN 
 B=1000
@@ -128,7 +128,20 @@ c(Tmean-2*sd_light/sqrt(len_sample), Tmean+2*sd_light/sqrt(len_sample))
 # the value of speed measured by Michelson and Newcomb is equal to the currently most accurate value, 299792.458 km/s
 t.test(light_vec, mu= 299792.458)
 
-### Ex 4
+### Exercise 3
+# Question 3.1
+klm=scan("klm.txt")
+qqnorm(klm,main="QQ-plot of KLM")
+m=sum(klm>31)
+n= length(klm)
+binom.test(m,n,p=0.5)
+
+# Question 3.2
+m=sum(klm>72)
+n= length(klm)
+binom.test(m,n,p=0.1)
+
+### Exercise 4
 # Question 4.1
 par(mfrow=c(1,4))
 cloud_data = read.table("clouds.txt", header=TRUE)
@@ -217,9 +230,9 @@ wilcox.test(square_root_of_square_root_data$seeded, square_root_of_square_root_d
 #  Kolmogorov-Smirnov test: p-value = 0.01905 < 0.05, we can conclude that Ho of equal means is rejected
 ks.test(square_root_of_square_root_data$seeded, square_root_of_square_root_data$unseeded)
 
-#Exercise 5
+###Exercise 5
 
-#Item 1
+# Question 5.1
 peruvians=read.table("peruvians.txt",header=TRUE)
 peruvians = peruvians[,-c(5,6,7)]
 pairs(peruvians, upper.panel=NULL)
@@ -228,7 +241,7 @@ pairs(peruvians, upper.panel=NULL)
 # This conclusion is based on the fact that the plot from these pairs resembles a straight line passing through the
 # orign.
 
-#Item 2
+# Question 5.2
 # Tests will be conducted using Spearma's rank correlation test which doesn't assume normality between the two
 # variables.
 attach(peruvians) # Keeping the dataset in memory so r functions will access information based on column's
@@ -268,3 +281,109 @@ cor.test(migration, diastolic,method="spearman")
 # Answer: As can be seen from the test, p-value = 0.6494, which leds us to fail to reject the null hypothesis that rho is equal
 # to 0 - considering a o.05 confidence level (despite the fact that R's output states that H0 can be rejected).
 # However, the calculated rho for the sample is different from 0 (in fact rho = 0.0751).
+
+### Exercise 6
+# Question 6.1
+run=read.table("run.txt")
+run
+par(mfrow=c(1,2))
+qqnorm(soft_drink[,1],main = 'QQ-Plot Before Soft Drink')
+qqnorm(soft_drink[,2],main = 'QQ-Plot After Soft Drink')
+hist(soft_drink[,1],main = 'Before Soft Drink')
+hist(soft_drink[,2],main = 'After Soft Drink')
+boxplot(soft_drink[,1],main = 'Before Soft Drink')
+boxplot(soft_drink[,2],main = 'After Soft Drink')
+
+qqnorm(energy[,1],main = 'QQ-Plot Before Energy')
+qqnorm(energy[,2],main = 'QQ-Plot After Energy')
+hist(energy[,1],main = 'Before Energy')
+hist(energy[,2],main = 'After Energy')
+boxplot(energy[,1],main = 'Before Energy')
+boxplot(energy[,2],main = 'After Energy')
+
+# Question 6.2
+soft_drink=run[run$drink=='lemo', 1:2]
+energy=run[run$drink=='energy', 1:2]
+mystat=function(x,y) {mean(x-y)}
+B=1000
+
+
+#soft drink
+#t.test(soft_drink[,1],soft_drink[,2],paired = TRUE)
+
+soft_drink_tstar=numeric(B)
+for (i in 1:B){
+  soft_drink_star=t(apply(cbind(soft_drink[,1],soft_drink[,2]),1,sample))
+  soft_drink_tstar[i]=mystat(soft_drink_star[,1],soft_drink_star[,2])
+}
+soft_drink_myt=mystat(soft_drink_star[,1],soft_drink_star[,2])
+hist(soft_drink_tstar, main = 'Soft Drink T*')
+pl=sum(soft_drink_tstar<soft_drink_myt)/B
+pr=sum(soft_drink_tstar>soft_drink_myt)/B
+p_soft_drink=2*min(pl,pr)
+p_soft_drink
+
+
+#energy
+#t.test(energy[,1],energy[,2],paired = TRUE)
+
+energy_tstar=numeric(B)
+for (i in 1:B){
+  energy_star=t(apply(cbind(energy[,1],energy[,2]),1,sample))
+  energy_tstar[i]=mystat(energy_star[,1],energy_star[,2])
+}
+energy_myt=mystat(energy_star[,1],energy_star[,2])
+hist(energy_tstar, main = 'Energy T*')
+pl=sum(energy_tstar<energy_myt)/B
+pr=sum(energy_tstar>energy_myt)/B
+p_energy=2*min(pl,pr)
+p_energy
+
+# Question 6.3
+n=nrow(run)
+diff_time <- data.frame(numeric(n), character(n))
+diff_time[,1]= run[,2]- run[,1]
+diff_time[,2]= run[,3]
+wilcox.test(diff_time[1:12,1],diff_time[13:24,1])
+#diff_time
+
+# Question 6.4 
+# In Doc File
+
+# Question 6.5 
+# In Doc File
+
+# Question 6.6
+par(mfrow=c(1,2))
+qqnorm(diff_time[1:12,1],main = 'QQ-Plot of Residual lemo')
+qqnorm(diff_time[13:24,1],main = 'QQ-Plot of Residual soft')
+t.test(diff_time[1:12,1],diff_time[13:24,1],paired = TRUE)
+
+### Exercise 7
+
+# Question 7.1
+dogs=read.table("dogs.txt",header = TRUE)
+dogs
+par(mfrow=c(1,3))
+boxplot(dogs[,1], main = "V1: isofluorane")
+boxplot(dogs[,2], main = "V2: halothane")
+boxplot(dogs[,3], main = "V3: cyclopropane")
+
+qqnorm(dogs[,1], main = "QQ-plot V1: isofluorane")
+qqnorm(dogs[,2], main = "QQ-plot V2: halothane")
+qqnorm(dogs[,3], main = "QQ-plot V3: cyclopropane")
+
+# Question 7.2
+dogsframe= data.frame(plasma=as.vector(as.matrix(dogs)), drugs=factor(rep(1:3,each=10)))
+dogsaov= lm(plasma~drugs,data = dogsframe)
+anova(dogsaov)
+summary(dogsaov)
+
+drug1 = 0.4340
+drug2 = drug1 - 0.0350
+drug3 = drug1 - 0.4190
+drug1; drug2; drug3
+
+# Question 7.3
+attach(dogsframe)
+kruskal.test(plasma,drugs)
